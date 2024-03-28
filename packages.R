19944d35b9fb23c9969ff96a7b4ddf55bebc37d3
FlorianKuschel <158284401+FlorianKuschel@users.noreply.github.com>
2024-03-28 09:51:48 +0100

    Create packages.R

---
+++ packages.R
@@ -0,0 +1,12 @@
+packages = c("metafor", "meta", "RVAideMemoire", "tidyverse", "readxl", "dplyr", "broom")
+
+## Now load or install & load all iff necessary
+package.check <- lapply(
+  packages,
+  FUN = function(x) {
+    if (!require(x, character.only = TRUE)) {
+      install.packages(x, dependencies = TRUE)
+      library(x, character.only = TRUE)
+    }
+  }
+)
